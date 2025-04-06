a69b69806fba0ea5ab038838e1e76ac7bfebb302
Cody Munger <codymunger@M1Mini.local>
2025-04-06 13:52:59 -0700

    static updates

--- backend/index.ts
+++ backend/index.ts
@@ -37,8 +37,6 @@ const accessLogStream = fs.createWriteStream(
 // setup the logger
 app.use(morgan('common', { stream: accessLogStream }))
 
-app.use(express.static('dist'))
-
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
@@ -51,8 +49,66 @@ sectigo.com
 67ef73c346636`;
 
 app.get('/.well-known/pki-validation/1FDA0D95DDB3FBF952FEDFA2526D9F02.txt', (req, res) => {
-	res.setHeader("Content-Type", "text/plain; charset=utf-8");
-	res.status(200).send(wellKnownContent);
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send(wellKnownContent);
+})
+
+function getStatic(file, res) {
+    console.log("static " + file + " called")
+    const pth = path.join("./dist/", file) 
+    fs.readFile(pth, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        let contentType = "";
+        if (file.endsWith(".html")) {
+            contentType = "text/html; charset=utf-8";
+        } else if (file.endsWith(".js")) {
+            contentType = "text/javascript; charset=utf-8";
+        } else if (file.endsWith(".css")) {
+            contentType = "text/css; charset=utf-8";
+        }
+
+        res.setHeader("Content-Type", contentType);
+        res.status(200).send(data)
+    })
+}
+
+app.get('/', (req, res) => {
+    console.log("static / called")
+    getStatic('/index.html', res);
+});
+
+app.get('', (req, res) => {
+    console.log("static '' called")
+    getStatic('/index.html', res);
+});
+
+app.get('/index.html', (req, res) => {
+    console.log("static /index.html called")
+    getStatic('/index.html', res);
+});
+
+app.get('/floppySnake.js', (req, res) => {
+    console.log("static /floppySnake.js called")
+    const pth = path.join("./dist/assets"); 
+    try {
+        fs.opendir(pth, (err, dir) => {
+            let entry;
+            while ((entry = dir.readSync()) != null) {
+                if (entry.name.endsWith('.js')) {
+                    getStatic("assets/" + entry.name, res);
+                    return;
+                }
+            };
+
+            res.status(500).send(err);
+        })
+    } catch(err) {
+        res.status(500).send(err);
+    }
 })
 
 app.get('/comment', (req, res) => {
