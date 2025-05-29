2d8d0dc72768a3e81648931f13c0116132e8141b
Cody Munger <mungerc@gmail.com>
2025-05-29 01:21:50 -0700

    borg

--- frontend/src/Sticker.ts
+++ frontend/src/Sticker.ts
@@ -5,7 +5,9 @@ const prefix = env.api + "/img/";
 export const Stickers = [
     prefix + "runningMouf.jpg",
     prefix + "chickenLegs.jpg",
-    prefix + "actionFigure.jpg"
+    prefix + "actionFigure.jpg",
+    prefix + "snek.jpg",
+    prefix + "borg.jpg",
 ];
 
 export function getStickerIndex(index: number):number {
