30de94997a19223b7c95e7990ddad9f55389df5f
Cody Munger <mungerc@gmail.com>
2025-06-09 00:03:40 -0700

    ip lookup opens in a new window

--- frontend/src/AdminMain.tsx
+++ frontend/src/AdminMain.tsx
@@ -256,7 +256,7 @@ export const IPEntry: React.FC<IPEntryProps> = ({
     const lookupUrl = "https://iplocation.io/ip/" + stripped;
 
     return (<div key={Math.random()} className="ipBlockDiv">
-        <div style={{ marginRight: 10 }}><a href={lookupUrl}>{stripped}</a></div>
+        <div style={{ marginRight: 10 }}><a target="_blank" rel="noopener noreferrer" href={lookupUrl}>{stripped}</a></div>
         <input type="checkbox" checked={ipData.blocked} onClick={() => {onBlocked(ipData.address)}} />
     </div>)
 }
